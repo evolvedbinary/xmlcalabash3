32db1c2ff11f321c6fd7364bdaa9985e2fb1d479
Norman Walsh <ndw@nwalsh.com>
2025-08-02 13:40:13 +0100

    Insiginificant whitespace/formatting changes to the source code

--- buildSrc/src/main/kotlin/ExternalDependencies.kt
+++ buildSrc/src/main/kotlin/ExternalDependencies.kt
@@ -3,109 +3,110 @@ package com.xmlcalabash.build
 import org.gradle.api.GradleException
 
 object ExternalDependencies {
-  val steps = mapOf<String,List<String>>(
-      "xmlcalabash" to listOf(
-          "org.jetbrains.kotlin:kotlin-stdlib:2.1.20",
-          "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2",
-          "org.jetbrains.kotlin:kotlin-reflect:2.1.20",
+    val steps = mapOf<String,List<String>>(
+        "xmlcalabash" to listOf("org.jetbrains.kotlin:kotlin-stdlib:2.1.20",
+            "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2",
+            "org.jetbrains.kotlin:kotlin-reflect:2.1.20",
 
-          "com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.18.2",
-          "com.fasterxml.jackson.dataformat:jackson-dataformat-toml:2.18.2",
-          "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.2",
-          "com.github.f4b6a3:uuid-creator:6.0.0",
-          "com.networknt:json-schema-validator:1.5.4",
-          "com.nwalsh:sinclude:5.4.1",
-          "com.vladsch.flexmark:flexmark-all:0.64.8",
-          "commons-codec:commons-codec:1.17.0",
-          "javax.activation:activation:1.1.1", // For mimetype mapping
-          "nu.validator:htmlparser:1.4.16",
-          "org.apache.commons:commons-compress:1.27.1",
-          "org.apache.httpcomponents.client5:httpclient5:5.3.1",
-          "org.brotli:dec:0.1.2",
-          "org.jline:jline-reader:3.28.0",
-          "org.jline:jline-terminal-jni:3.28.0",
-          "org.jline:jline-terminal:3.28.0",
-          "org.nineml:coffeefilter:3.2.9",
-          "org.nineml:coffeegrinder:3.2.9",
-          "org.relaxng:jing:20241231",
-          "org.tukaani:xz:1.10",
-          "org.xmlresolver:xmlresolver:6.0.17",
+            "com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.18.2",
+            "com.fasterxml.jackson.dataformat:jackson-dataformat-toml:2.18.2",
+            "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.2",
+            "com.github.f4b6a3:uuid-creator:6.0.0",
+            "com.networknt:json-schema-validator:1.5.4",
+            "com.nwalsh:sinclude:5.4.1",
+            "com.vladsch.flexmark:flexmark-all:0.64.8",
+            "commons-codec:commons-codec:1.17.0",
+            "javax.activation:activation:1.1.1", // For mimetype mapping
+            "nu.validator:htmlparser:1.4.16",
+            "org.apache.commons:commons-compress:1.27.1",
+            "org.apache.httpcomponents.client5:httpclient5:5.3.1",
+            "org.brotli:dec:0.1.2",
+            "org.jline:jline-reader:3.28.0",
+            "org.jline:jline-terminal-jni:3.28.0",
+            "org.jline:jline-terminal:3.28.0",
+            "org.nineml:coffeefilter:3.2.9",
+            "org.nineml:coffeegrinder:3.2.9",
+            "org.relaxng:jing:20241231",
+            "org.tukaani:xz:1.10",
+            "org.xmlresolver:xmlresolver:6.0.17",
 
-          // I was using log4j but httpclient5 uses slf4j.
-          // Could I get httpclient5 to use log4j? Maybe. ¯\_(ツ)_/¯
-          // But I got tired of trying to figure it out so I did this instead.
-          "org.slf4j:slf4j-api:2.0.16",
-          "ch.qos.logback:logback-classic:1.5.13",
-          "org.apache.logging.log4j:log4j-to-slf4j:2.23.1"),
+            // I was using log4j but httpclient5 uses slf4j.
+            // Could I get httpclient5 to use log4j? Maybe. ¯\_(ツ)_/¯
+            // But I got tired of trying to figure it out so I did this instead.
+            "org.slf4j:slf4j-api:2.0.16",
+            "ch.qos.logback:logback-classic:1.5.13",
+            "org.apache.logging.log4j:log4j-to-slf4j:2.23.1"),
 
-      "asciidoctor" to listOf("org.asciidoctor:asciidoctorj:3.0.0",
-                              "org.asciidoctor:asciidoctorj-pdf:2.3.19"),
+        "asciidoctor" to listOf("org.asciidoctor:asciidoctorj:3.0.0",
+            "org.asciidoctor:asciidoctorj-pdf:2.3.19"),
 
-      "diagramming" to listOf("net.sourceforge.plantuml:plantuml:1.2025.2",
-                              // n.b., plantuml includes ditaa 0.11.x
-                              "de.rototor.jeuclid:jeuclid-core:3.1.14"),
+        "diagramming" to listOf("net.sourceforge.plantuml:plantuml:1.2025.2", // plantuml includes ditaa
+            "de.rototor.jeuclid:jeuclid-core:3.1.14"),
 
-      "plantuml" to listOf("net.sourceforge.plantuml:plantuml:1.2025.2"),
-      "ditaa" to listOf("net.sourceforge.plantuml:plantuml:1.2025.2"),
-      "mathml-to-svg" to listOf("de.rototor.jeuclid:jeuclid-core:3.1.14"),
+        "plantuml" to listOf("net.sourceforge.plantuml:plantuml:1.2025.2"),
 
-      "ebnf-convert" to listOf("de.bottlecaps.ebnf-convert:ebnf-convert-lib:0.70"),
+        "ditaa" to listOf("net.sourceforge.plantuml:plantuml:1.2025.2"),
 
-      "epubcheck" to listOf("org.w3c:epubcheck:5.0.1",
-                            "javax.activation:activation:1.1.1"), // For mimetype mapping
+        "mathml-to-svg" to listOf("de.rototor.jeuclid:jeuclid-core:3.1.14"),
 
-      "find" to listOf("com.jayway.jsonpath:json-path:2.9.0",
-                       "net.minidev:json-smart:2.5.1"),
+        "ebnf-convert" to listOf("de.bottlecaps.ebnf-convert:ebnf-convert-lib:0.70"),
 
-      "jsonpath" to listOf("com.jayway.jsonpath:json-path:2.9.0",
-                           "net.minidev:json-smart:2.5.1"),
+        "epubcheck" to listOf("org.w3c:epubcheck:5.0.1",
+            "javax.activation:activation:1.1.1"), // For mimetype mapping
 
-      "json-diff" to listOf("com.github.java-json-tools:json-patch:1.13"),
-      "json-patch" to listOf("com.github.java-json-tools:json-patch:1.13"),
+        "find" to listOf("com.jayway.jsonpath:json-path:2.9.0",
+            "net.minidev:json-smart:2.5.1"),
 
-      "markup-blitz" to listOf("de.bottlecaps:markup-blitz:1.6"),
+        "jsonpath" to listOf("com.jayway.jsonpath:json-path:2.9.0",
+            "net.minidev:json-smart:2.5.1"),
 
-      "metadata-extractor" to listOf("com.drewnoakes:metadata-extractor:2.19.0",
-                                     "org.apache.pdfbox:pdfbox:2.0.32",
-                                     "org.apache.pdfbox:xmpbox:2.0.32",
-                                     "javax.xml.bind:jaxb-api:2.3.1"), // pdfbox dep for parsing XML in pdfbox
+        "json-diff" to listOf("com.github.java-json-tools:json-patch:1.13"),
 
-      "paged-media-fop" to listOf("org.apache.xmlgraphics:fop:2.10",
-                                  "org.apache.avalon.framework:avalon-framework-api:4.3.1",
-                                  "org.apache.avalon.framework:avalon-framework-impl:4.3.1",
-                                  "javax.media:jai-core:1.1.3",
-                                  "com.sun.media:jai-codec:1.1.3"),
+        "json-patch" to listOf("com.github.java-json-tools:json-patch:1.13"),
 
-      "paged-media-prince" to listOf("com.princexml:prince-java-wrapper:1.3.0"),
+        "markup-blitz" to listOf("de.bottlecaps:markup-blitz:1.6"),
 
-      "polyglot" to listOf("org.graalvm.polyglot:polyglot:23.1.5",
-                           "org.graalvm.polyglot:js:23.1.5",
-                           "org.graalvm.polyglot:python:23.1.5"),
+        "metadata-extractor" to listOf("com.drewnoakes:metadata-extractor:2.19.0",
+            "org.apache.pdfbox:pdfbox:2.0.32", // pdfbox dep for parsing XML in pdfbox
+            "org.apache.pdfbox:xmpbox:2.0.32",
+            "javax.xml.bind:jaxb-api:2.3.1"),
 
-      "railroad" to listOf("de.bottlecaps.rr:rr-lib:2.1",
-                           "de.bottlecaps.ebnf-convert:ebnf-convert-lib:0.70"),
+        "paged-media-fop" to listOf("org.apache.xmlgraphics:fop:2.10",
+            "org.apache.avalon.framework:avalon-framework-api:4.3.1",
+            "org.apache.avalon.framework:avalon-framework-impl:4.3.1",
+            "javax.media:jai-core:1.1.3",
+            "com.sun.media:jai-codec:1.1.3"),
 
-      "rdf" to listOf("javax.activation:activation:1.1.1", // For mimetype mapping
-                      "org.apache.jena:jena:5.3.0",
-                      "org.apache.jena:jena-arq:5.3.0",
-                      "org.apache.jena:jena-core:5.3.0",
-                      "org.apache.jena:jena-iri:5.3.0",
-                      "org.semarglproject:semargl-core:0.7",
-                      "org.semarglproject:semargl-rdfa:0.7"),
+        "paged-media-prince" to listOf("com.princexml:prince-java-wrapper:1.3.0"),
 
-      "selenium" to listOf("org.nineml:coffeegrinder:3.2.9",
-                           "org.nineml:coffeefilter:3.2.9",
-                           "org.seleniumhq.selenium:selenium-java:4.28.1"),
+        "polyglot" to listOf("org.graalvm.polyglot:polyglot:23.1.5",
+            "org.graalvm.polyglot:js:23.1.5",
+            "org.graalvm.polyglot:python:23.1.5"),
 
-      "send-mail" to listOf("javax.activation:activation:1.1.1",
-                            "javax.mail:javax.mail-api:1.6.2",
-                            "com.sun.mail:javax.mail:1.6.2"),
+        "railroad" to listOf("de.bottlecaps.rr:rr-lib:2.1",
+            "de.bottlecaps.ebnf-convert:ebnf-convert-lib:0.70"),
 
-      "trang" to listOf("org.relaxng:trang:20241231"),
+        "rdf" to listOf("javax.activation:activation:1.1.1", // For mimetype mapping
+            "org.apache.jena:jena:5.3.0",
+            "org.apache.jena:jena-arq:5.3.0",
+            "org.apache.jena:jena-core:5.3.0",
+            "org.apache.jena:jena-iri:5.3.0",
+            "org.semarglproject:semargl-core:0.7",
+            "org.semarglproject:semargl-rdfa:0.7"),
 
-      "unique-id" to listOf("com.github.f4b6a3:uuid-creator:6.0.0"),
+        "selenium" to listOf("org.nineml:coffeegrinder:3.2.9",
+            "org.nineml:coffeefilter:3.2.9",
+            "org.seleniumhq.selenium:selenium-java:4.28.1"),
 
-      "xmlunit" to listOf("org.xmlunit:xmlunit-core:2.10.0"))
+        "send-mail" to listOf("javax.activation:activation:1.1.1",
+            "javax.mail:javax.mail-api:1.6.2",
+            "com.sun.mail:javax.mail:1.6.2"),
+
+        "trang" to listOf("org.relaxng:trang:20241231"),
+
+        "unique-id" to listOf("com.github.f4b6a3:uuid-creator:6.0.0"),
+
+        "xmlunit" to listOf("org.xmlunit:xmlunit-core:2.10.0"))
 
   fun of(keys: List<String>): List<String> {
     val unifiedDeps = mutableSetOf<String>()
