e6f4f3629e2a6418d9e3c472422fee59ebc87999
Norman Walsh <ndw@nwalsh.com>
2025-11-23 10:58:55 +0000

    Improved error explanations
    Attempted to improve the line breakinging. It’s now customizable, at least at
    the API level, and reduced from ~80 to 72.
    
    Fixed typos in the error explanations.
    
    Added an explanation for err:XS0029.

--- xmlcalabash/src/main/kotlin/com/xmlcalabash/exceptions/ErrorExplanation.kt
+++ xmlcalabash/src/main/kotlin/com/xmlcalabash/exceptions/ErrorExplanation.kt
@@ -2,6 +2,7 @@ package com.xmlcalabash.exceptions
 
 interface ErrorExplanation {
     var showStacktrace: Boolean
+    var messageWidth: Int
     fun report(error: XProcError)
     fun reportExplanation(error: XProcError)
     fun message(error: XProcError, includeDetails: Boolean): String
