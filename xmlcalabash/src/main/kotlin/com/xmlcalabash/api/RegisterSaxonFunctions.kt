063aa7d8427ef770e22191c4f7cc8242e4ad2c8f
Norman Walsh <ndw@nwalsh.com>
2026-01-03 09:40:15 +0000

    Copy the Saxon catalogs when running a pipeline function
    Fix #491
    
    When running a pipeline function from outside XML Calabash, copy the catalog
    files from the Saxon configuration into the XML Calabash configuration. This
    allows the pipeline function to use any -catalogs specified for Saxon.

--- xmlcalabash/src/main/kotlin/com/xmlcalabash/api/RegisterSaxonFunctions.kt
+++ xmlcalabash/src/main/kotlin/com/xmlcalabash/api/RegisterSaxonFunctions.kt
@@ -6,8 +6,11 @@ import com.xmlcalabash.exceptions.XProcException
 import com.xmlcalabash.namespace.NsErr
 import com.xmlcalabash.util.UriUtils
 import net.sf.saxon.Configuration
+import net.sf.saxon.lib.CatalogResourceResolver
 import net.sf.saxon.lib.Initializer
 import org.apache.logging.log4j.kotlin.logger
+import org.xmlresolver.ResolverFeature
+import java.net.URI
 import kotlin.collections.iterator
 
 class RegisterSaxonFunctions(): Initializer {
@@ -19,6 +22,23 @@ class RegisterSaxonFunctions(): Initializer {
         val libraryUri = System.getProperty("com.xmlcalabash.pipelines")
         if (libraryUri != null) {
             val invocation = XmlCalabashBuilder()
+
+            if (config.resourceResolver is CatalogResourceResolver) {
+                val resolver = config.resourceResolver as CatalogResourceResolver
+                val catalogs = mutableListOf<URI>()
+                catalogs.addAll(invocation.xmlCatalogs.getOrDefault() ?: emptyList())
+                for (catalog in resolver.getFeature(ResolverFeature.CATALOG_FILES)) {
+                    val uri = UriUtils.cwdAsUri().resolve(catalog)
+                    catalogs.add(uri)
+                    logger.debug { "Adding ${uri} to list of catalogs" }
+                }
+                for (catalog in resolver.getFeature(ResolverFeature.CATALOG_ADDITIONS)) {
+                    val uri = UriUtils.cwdAsUri().resolve(catalog)
+                    catalogs.add(uri)
+                    logger.debug { "Adding ${uri} to list of catalogs" }
+                }
+                invocation.xmlCatalogs.set(catalogs)
+            }
 
             val calabashConfig = System.getProperty("com.xmlcalabash.configuration")
             if (calabashConfig != null) {
